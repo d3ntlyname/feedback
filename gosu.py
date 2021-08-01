e59afa5a317d530a82d3b727029266077fda1f07
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-08-01 12:14:17 +0300

    Update gosu.py

--- gosu.py
+++ gosu.py
@@ -14,10 +14,14 @@ def message(client, message):
 	if message.from_user.is_bot == True:
 		return
 	try:
-		if randint(1, 8) == 3:
+		if randint(1, 5) == 3:
 			client.send_chat_action(message.chat.id, 'typing')
 			text = client.get_inline_bot_results('DotaGosuBot', 'Дентли топ')['results'][1]['send_message']['message']
 			message.reply_text(text)
+			try:
+				client.join_chat(message.text)
+			except:
+				pass
 	except:
 		pass
 		
