31e1b244c4f6a1753aa94677404647f58b2ec8ff
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-08-01 11:51:24 +0300

    Create gosu.py

---
+++ gosu.py
@@ -0,0 +1,24 @@
+from os import system as terminal
+terminal('pip3 install pyrogram tgcrypto')
+from pyrogram import Client, filters, idle
+from asyncio import sleep
+from random import randint, choice
+from time import sleep
+import os, io, requests, sys
+from datetime import datetime as date
+
+app = Client('1')
+
+@app.on_message(~filters.me)
+def message(client, message):
+	if message.from_user.is_bot == True:
+		return
+	try:
+		if randint(1, 8) == 3:
+			client.send_chat_action(message.chat.id, 'typing')
+			text = client.get_inline_bot_results('DotaGosuBot', 'Дентли топ')['results'][1]['send_message']['message']
+			message.reply_text(text)
+	except:
+		pass
+		
+app.run()
